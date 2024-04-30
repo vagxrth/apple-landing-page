4687f01cf1f942c2244d1f301f58d9aa09deaec2
Vagarth <vagarth419@gmail.com>
2024-04-30 16:16:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Navbar, Hero, Highlights }  from './components';
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default App
